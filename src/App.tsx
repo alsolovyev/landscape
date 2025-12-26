58f68f1ea1a0c679692bc046bccbb2d1f6759104
Aleksey Solovyev <solovyev.a@icloud.com>
2025-12-26 19:45:33 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import Layout from "./Layout";
 import Home from "./Pages/Home";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { PenBox } from "lucide-react";
 
 function App() {
+  const [isEditable, setIsEditable] = useState(false);
+
+  function toggleEditable() {
+    setIsEditable((prev) => !prev);
+    document.body.contentEditable = !isEditable;
+  }
+
   return (
     <Layout currentPageName="Home">
+      <Button
+        onClick={toggleEditable}
+        size="lg"
+        className="fixed top-4 left-4 p-4 z-50"
+      >
+        <PenBox />
+      </Button>
       <Home />
     </Layout>
   );
